bbc3b883a0ea51eda304dddaf8a2a177ed35ae5a
Nico Deblauwe <nico@bluepundit.eu>
2020-09-07 15:28:54 +0200

    remove old files

--- resources/views/components/OLDformelements/hidden.blade.php
+++
@@ -1,9 +0,0 @@
-{{-- resources/views/components/forms/hidden.blade.php --}}
-
-@php
-	$id_field = (isset($id)) ? $id : 'id';
-	$vars = get_defined_vars();
-	$value = (array_key_exists('value', $vars)) ? $value : ( (isset($m)) ? $m->$id_field : null);
-@endphp
-
-<input type="hidden" class="form-control" id="{{$id}}" name="{{$id}}" value="{{$value}}">

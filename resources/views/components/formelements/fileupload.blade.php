283b1abe591585b3d8eba3bb215d4368673cb605
Nico Deblauwe <nico@bluepundit.eu>
2020-12-15 09:55:27 +0100

    show the uploaded file

--- resources/views/components/formelements/fileupload.blade.php
+++ resources/views/components/formelements/fileupload.blade.php
@@ -2,6 +2,17 @@
     <div class="form-group">
         <label for="{{ $name }}">{!! $label !!}&nbsp;{!! $required  !!}</label>
 
+        @if( isset($model) && ($model->$name !== null) )
+            <div class="row">
+                <div class="col-4">
+                    <a href="">{{ $model->filename_original }}</a>
+                </div>
+                <div class="col-8">
+                    <span class="text-muted">Uploading a new {{$name}} will automatically remove this file.</span>
+                </div>
+            </div>
+        @endif
+
         <input
                 type="file"
                 name="{{$id}}"

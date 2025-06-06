8ee8129ddbfae5df0c3ab286317280d882f201bf
Nico Deblauwe <nico@bluepundit.eu>
2025-06-06 17:46:20 +0200

    Add possibility to add info on edit screen

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -2,36 +2,46 @@
 
 @section('main')
 
-    <form role="form" action="{{ route('blueadmin.update', ['modelname' => $modelname, 'id' => $m->id]) }}" method="POST" class="horizontal" enctype="multipart/form-data">
+    <div class="row">
+        <div class="col-12 col-md-7">
 
-        <x-blueadmin-card :title="'Edit <strong>'.$title . '</strong> record ' . $m->id " icon="far fa-edit" col="col-12 col-md-7">
+            <form role="form" action="{{ route('blueadmin.update', ['modelname' => $modelname, 'id' => $m->id]) }}" method="POST" class="horizontal" enctype="multipart/form-data">
 
-            @csrf
-            @method('PUT')
+                <x-blueadmin-card :title="'Edit <strong>'.$title . '</strong> record ' . $m->id " icon="far fa-edit" col="col-12">
 
-            @bind($m ?? null)
-                @include('admin.' . $modelname .'._form')
-            @endbind
+                    @csrf
+                    @method('PUT')
 
-            <x-slot name="cardTools">
-                <span class="font-weight-light">Fields with a <span class="text-primary">*</span> are required</span>
-            </x-slot>
+                    @bind($m ?? null)
+                        @include('admin.' . $modelname .'._form')
+                    @endbind
 
-            <x-slot name="footer">
-                <div class="float-right">
-                    <a href="{{ Session::get('blueadmin.returnpath', route('blueadmin.index', $modelname) ) }}" class="btn btn-default">Cancel</a>&nbsp;&nbsp;
-                    <button type="submit" class="btn btn-primary">Save changes</button>
-                </div>
-            </x-slot>
+                    <x-slot name="cardTools">
+                        <span class="font-weight-light">Fields with a <span class="text-primary">*</span> are required</span>
+                    </x-slot>
 
-        </x-blueadmin-card>
+                    <x-slot name="footer">
+                        <div class="float-right">
+                            <a href="{{ Session::get('blueadmin.returnpath', route('blueadmin.index', $modelname) ) }}" class="btn btn-default">Cancel</a>&nbsp;&nbsp;
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </x-slot>
 
-    </form>
+                </x-blueadmin-card>
 
+            </form>
+        </div>
+
+        @if(View::exists('admin.'.$modelname.'._edit-side'))
+        <div class="col-12 col-md-4">
+            @include('admin.'.$modelname.'._edit-side', ['m' => $m])
+        </div>
+        @endif
+    </div>
 @endsection
 
 @if(View::exists('admin.'.$modelname.'._aside-form'))
-@section('aside')
-@include('admin.'.$modelname.'._aside-form')
-@endsection
+    @section('aside')
+        @include('admin.'.$modelname.'._aside-form')
+    @endsection
 @endif
